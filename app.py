f7eec4e629a680434f0f11e1f5ee1fc00af7afc7
putrilikaa <putrilikawati487@gmail.com>
2024-06-28 08:56:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ if selected == 'Manual Input':
 elif selected == 'File Upload':
     st.title('Transaction Prediction - File Upload')
 
-    uploaded_file = st.file_uploader("**Upload file excel yang berisi data TX_AMOUNT dan TX_TIME_SECONDS**", type=["xlsx"])
+    uploaded_file = st.file_uploader("Upload file excel yang berisi data TX_AMOUNT dan TX_TIME_SECONDS", type=["xlsx"])
 
     if uploaded_file is not None:
         try:
@@ -168,7 +168,7 @@ elif selected == 'Pemodelan Random Forest':
 
     st.write("Halaman ini digunakan untuk evaluasi model menggunakan data yang berbeda, tidak terkait dengan data yang diupload sebelumnya.")
 
-    uploaded_file_rf = st.file_uploader("**Upload file excel yang berisi data TX_AMOUNT, TX_TIME_SECONDS dan FRAUD**", type=["xlsx"])
+    uploaded_file_rf = st.file_uploader("Upload file excel yang berisi data TX_AMOUNT, TX_TIME_SECONDS dan FRAUD", type=["xlsx"])
 
     if uploaded_file_rf is not None:
         try:
@@ -260,3 +260,6 @@ elif selected == 'Info':
     - *AUC ROC (Area Under the Receiver Operating Characteristic Curve)* mengukur kinerja model klasifikasi pada berbagai threshold keputusan.
     - *ROC (Receiver Operating Characteristic Curve)* adalah grafik yang menggambarkan rasio True Positive Rate (Sensitivitas) terhadap False Positive Rate (1 - Spesifisitas) untuk berbagai nilai threshold.
     """)
+
+# Matplotlib deprecation warning
+st.set_option('deprecation.showPyplotGlobalUse', False)
